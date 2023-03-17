e52eb800c8d79c477916714c2b72afb5df933e94
Stanislav <people.ss@yandex.ru>
2023-03-17 03:54:06 +0300

    Rename percent to Percent

--- src/main/java/HomeworkJavaMvn/BonusService.java
+++ src/main/java/HomeworkJavaMvn/BonusService.java
@@ -2,8 +2,8 @@ package HomeworkJavaMvn;
 
 public class BonusService {
     public long calculate (long amount, boolean registered) {
-        long percent = registered ? 3 : 1;
-        long bonus = amount * percent / 100;
+        long Percent = registered ? 3 : 1;
+        long bonus = amount * Percent / 100;
         if (bonus > 500) {
             bonus = 500;
         }
