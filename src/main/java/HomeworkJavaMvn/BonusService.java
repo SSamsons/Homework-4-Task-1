49f5a2d96a1b3656e8129dc81353d55106d4b3b1
Stanislav <people.ss@yandex.ru>
2023-03-17 04:16:36 +0300

    Fix #1

--- src/main/java/HomeworkJavaMvn/BonusService.java
+++ src/main/java/HomeworkJavaMvn/BonusService.java
@@ -2,8 +2,8 @@ package HomeworkJavaMvn;
 
 public class BonusService {
     public long calculate (long amount, boolean registered) {
-        long Percent = registered ? 3 : 1;
-        long bonus = amount * Percent / 100;
+        long percent = registered ? 3 : 1;
+        long bonus = amount * percent / 100;
         if (bonus > 500) {
             bonus = 500;
         }
